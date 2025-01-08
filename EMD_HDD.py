4a59003222e9f26a3f3ed11b2538fa10d352e47e
eladlavi29 <47532871+eladlavi29@users.noreply.github.com>
2025-01-08 12:26:49 +0200

    Update EMD_HDD.py

--- EMD_HDD.py
+++ EMD_HDD.py
@@ -17,6 +17,32 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 cpu = torch.device("cpu")
 
 
+def whole_pipeline_all(X,y, rows_factor, cols_factor, is_normalize_each_band=True, method_label_patch='most_common', random_seed=None, method_type = consts.REGULAR_METHOD, distances_bands=None, precomputed_distances = None):
+        print("XXXXXXX IN METHOD XXXXXXXXX")
+        st = time.time()
+
+        my_HDD_HDE = HDD_HDE(X,y, rows_factor, cols_factor, is_normalize_each_band, method_label_patch, method_type, distances_bands, precomputed_distances=precomputed_distances)
+        d_HDD, labels_padded, num_patches_in_row,y_patches = my_HDD_HDE.calc_hdd()
+        
+        print("WHOLE METHOD TIME: ", time.time()-st, flush=True)
+        st = time.time()
+
+        print("XXXXXXX IN CLASSIFICATION XXXXXXXXX")
+
+        y_patches = y_patches.int()
+        
+        if torch.cuda.is_available():
+            d_HDD = d_HDD.cpu()
+            y_patches = y_patches.cpu()
+            labels_padded = labels_padded.cpu()
+
+        clf = PaviaClassifier(d_HDD.numpy(), y_patches.numpy(), consts.N_NEIGHBORS, labels_padded.numpy(), rows_factor, cols_factor, num_patches_in_row, is_divided=False, random_seed = random_seed)
+
+        return clf.classify()
+
+        # print("WHOLE CLASSIFICATION TIME: ", time.time()-st)
+        
+
 def wasser_hdd(X,y, rows_factor, cols_factor, is_normalize_each_band=True, method_label_patch='most_common', random_seed=None, M='', precomputed_pack=None):
     if precomputed_pack is None:
             if M!='hdd' and M!='euclidean':
